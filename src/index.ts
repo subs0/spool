8f22518e67e1a00dfa25a23bc9c256a7f57b2981
Logan Powell <loganp@tepper.cmu.edu>
2020-02-12 07:32:21 -0500

    exports

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,7 @@ export { createSetStateCMD, SET_STATE } from "./commands"
 
 export { run$, command$, out$, task$ } from "./core"
 
-export {
-  registerCMD,
-  // registerRouter,
-  router
-} from "./registers"
+export { registerCMD, registerCMDtoStore, router } from "./registers"
 
 export { $store$, set$$tate } from "./store"
 
