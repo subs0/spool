e31c4f8520748fa2a6bacf0cc673d215a501c003
Logan Powell <loganp@tepper.cmu.edu>
2020-02-13 10:50:44 -0500

    switching to subtree directory and running updates

--- lib/core/multiplex.d.ts
+++ lib/core/multiplex.d.ts
@@ -1,5 +1,6 @@
-export declare const run$: import("@thi.ng/rstream").PubSub<unknown, unknown>;
-export declare const out$: import("@thi.ng/rstream").PubSub<unknown, unknown>;
-export declare const command$: import("@thi.ng/rstream").Subscription<unknown, unknown>;
+import { Subscription, PubSub } from "@thi.ng/rstream";
+export declare const run$: PubSub<any, any>;
+export declare const out$: PubSub<any, any>;
+export declare const command$: Subscription<any, any>;
 export declare const multiplex: (task_array: any) => any;
-export declare const task$: import("@thi.ng/rstream").Subscription<unknown, unknown>;
+export declare const task$: Subscription<any, any>;
--- lib/registers/registerCMD.d.ts
+++ lib/registers/registerCMD.d.ts
@@ -1,5 +1,4 @@
 import { IAtom } from "@thi.ng/atom";
-import { ISubscribable } from "@thi.ng/rstream";
-export declare const supplement$CMD: (cmd: Object, to$: ISubscribable<any>) => import("@thi.ng/rstream").Subscription<any, any>;
+export declare const supplement$CMD: any;
 export declare const registerCMDtoStore: (store: IAtom<any>) => (command?: Object) => any;
 export declare const registerCMD: (command?: Object) => any;
