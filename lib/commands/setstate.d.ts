27a326fb2f4c4c3b0c9a3554fb84a6eb5fac27c0
Logan Powell <loganp@tepper.cmu.edu>
2020-02-13 10:15:21 -0500

    patch spool

--- lib/commands/setstate.d.ts
+++ lib/commands/setstate.d.ts
@@ -1,5 +1,8 @@
+import { CMD_SUB$, CMD_ARGS, CMD_WORK } from "@-0/keys";
 export declare const createSetStateCMD: (store: any) => any;
 export declare const SET_STATE: any;
 export declare const set$$tateHOC: (store: any) => {
-    [x: string]: string | ((x: any) => any) | ((args: any) => any);
+    [CMD_WORK]: (args: any) => any;
+    [CMD_SUB$]: string;
+    [CMD_ARGS]: (x: any) => any;
 };
