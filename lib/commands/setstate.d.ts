f57813436048d13a114649ae7c46883a11aa2019
Logan Powell <loganp@tepper.cmu.edu>
2020-02-13 10:11:33 -0500

    dealing with the fam

--- lib/commands/setstate.d.ts
+++ lib/commands/setstate.d.ts
@@ -1,8 +1,5 @@
-import { CMD_SUB$, CMD_ARGS, CMD_WORK } from "@-0/keys";
 export declare const createSetStateCMD: (store: any) => any;
 export declare const SET_STATE: any;
 export declare const set$$tateHOC: (store: any) => {
-    [CMD_WORK]: (args: any) => any;
-    [CMD_SUB$]: string;
-    [CMD_ARGS]: (x: any) => any;
+    [x: string]: string | ((x: any) => any) | ((args: any) => any);
 };
