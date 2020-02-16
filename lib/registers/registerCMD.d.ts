5e45d2852a9661479546cd60fcc343494bd9afeb
Logan Powell <loganp@tepper.cmu.edu>
2020-02-16 02:40:49 -0500

    syncing supertree with subtree

--- lib/registers/registerCMD.d.ts
+++ lib/registers/registerCMD.d.ts
@@ -1,12 +1,12 @@
 import { CMD_SUB$, CMD_ARGS, CMD_RESO, CMD_ERRO, Command } from "@-0/keys";
 export declare const supplement$CMD: any;
 export declare const registerCMD: (command?: Command) => {
-    [CMD_SUB$]: string;
+    [CMD_SUB$]: any;
     [CMD_ARGS]: any;
-    [CMD_RESO]: (acc: Object, res: Object) => any;
-    [CMD_ERRO]: (acc: Object, err: Error) => any;
+    [CMD_RESO]: any;
+    [CMD_ERRO]: any;
 } | {
-    [CMD_SUB$]: string;
+    [CMD_SUB$]: any;
     [CMD_ARGS]: any;
     reso?: undefined;
     erro?: undefined;
