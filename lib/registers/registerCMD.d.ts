903c0d7e7dd836cf9a005057e13f4742f73a8ce5
Logan Powell <loganp@tepper.cmu.edu>
2020-02-27 12:41:01 -0500

    switching to subtree directory and running updates

--- lib/registers/registerCMD.d.ts
+++ lib/registers/registerCMD.d.ts
@@ -1,13 +1,13 @@
 import { CMD_SUB$, CMD_ARGS, CMD_RESO, CMD_ERRO, Command } from "@-0/keys";
 export declare const supplement$CMD: any;
 export declare const registerCMD: (command?: Command) => {
-    [CMD_SUB$]: any;
-    [CMD_ARGS]: any;
-    [CMD_RESO]: any;
-    [CMD_ERRO]: any;
+    sub$: any;
+    args: any;
+    reso: any;
+    erro: any;
 } | {
-    [CMD_SUB$]: any;
-    [CMD_ARGS]: any;
+    sub$: any;
+    args: any;
     reso?: undefined;
     erro?: undefined;
 };
