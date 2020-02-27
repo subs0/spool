b6f5810eeab022a04124c8f142669e952cbb6671
Logan Powell <loganp@tepper.cmu.edu>
2020-02-27 12:18:14 -0500

    updates

--- lib/store/state.d.ts
+++ lib/store/state.d.ts
@@ -1,3 +1,3 @@
 import { IAtom } from "@thi.ng/atom";
 export declare const $store$: IAtom<any>;
-export declare const set$$tate: (path: any, val: any, store?: IAtom<any>) => any;
+export declare const set$$tate: (path: any, val: any, store?: any) => any;
