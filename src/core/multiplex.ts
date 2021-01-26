9977f9769e9afd7b23364fc7a16d569ee1003e76
Logan Powell <loganp@tepper.cmu.edu>
2021-01-26 13:30:40 -0500

    prettier

--- src/core/multiplex.ts
+++ src/core/multiplex.ts
@@ -23,9 +23,9 @@ import { getIn } from "@thi.ng/paths"
  *
  */
 export const run$: PubSub<any, any> = pubsub({
-  topic: (x) => !!x[CMD_SUB$],
-  id: "run$_stream",
-  equiv: (x, y) => x === y || y === "_TRACE_STREAM",
+    topic: (x) => !!x[CMD_SUB$],
+    id: "run$_stream",
+    equiv: (x, y) => x === y || y === "_TRACE_STREAM",
 })
 
 /**
@@ -33,9 +33,9 @@ export const run$: PubSub<any, any> = pubsub({
  * for responding to emmitted Commands
  */
 export const out$: PubSub<any, any> = pubsub({
-  topic: (x) => x[CMD_SUB$],
-  id: "out$_stream",
-  equiv: (x, y) => x === y || y === "_TRACE_STREAM",
+    topic: (x) => x[CMD_SUB$],
+    id: "out$_stream",
+    equiv: (x, y) => x === y || y === "_TRACE_STREAM",
 })
 
 /**
@@ -46,12 +46,12 @@ export const out$: PubSub<any, any> = pubsub({
  * simple lookup of the `sub$` key of the command
  */
 export const command$: Subscription<any, any> = run$.subscribeTopic(
-  true,
-  {
-    next: (x) => out$.next(x),
-    error: console.warn,
-  },
-  { id: "command$_stream" }
+    true,
+    {
+        next: (x) => out$.next(x),
+        error: console.warn,
+    },
+    { id: "command$_stream" }
 )
 
 /**
@@ -61,18 +61,18 @@ export const command$: Subscription<any, any> = run$.subscribeTopic(
  *
  */
 export const task$: Subscription<any, any> = run$.subscribeTopic(
-  false,
-  {
-    next: multiplex,
-    error: console.warn,
-  },
-  { id: "task$_stream" }
+    false,
+    {
+        next: multiplex,
+        error: console.warn,
+    },
+    { id: "task$_stream" }
 )
 
 const err_str = "Spooling Interupted" // <- add doc link to error strings
 
 const nosub$_err = (c, i) =>
-  console.warn(`
+    console.warn(`
   🔥 No sub$ included for a Command with a primitive for 'args'. 
   🔥 Ergo, nothing was done with this Command: 
   
@@ -192,117 +192,117 @@ const nosub$_err = (c, i) =>
  *
  */
 export function multiplex(task_array) {
-  return task_array.reduce(async (a, c, i) => {
-    const acc = await a
-    // console.log("ACCUMULATOR =>", acc)
-    if (isFunction(c)) {
-      try {
-        const recur = c(acc)
-        // ensures accumulator is preserved between stacks
-        recur.unshift({ [CMD_ARGS]: acc })
-        return multiplex(recur)
-      } catch (e) {
-        console.warn(err_str, e)
-        return
-      }
-    }
-    const sub$ = c[CMD_SUB$]
-    const args = c[CMD_ARGS]
-    const erro = c[CMD_ERRO]
-    const reso = c[CMD_RESO]
-    // const _source$ = c[source$]
-    // const _handler = c[handler]
-    const knowns = [CMD_SUB$, CMD_ARGS, CMD_RESO, CMD_ERRO, CMD_SRC$, CMD_WORK]
-    const [unknowns] = diff_keys(knowns, c)
-
-    if (unknowns.length > 0) throw new Error(xKeyError(err_str, c, unknowns, sub$, i))
-    const arg_type = stringify_type(args)
-
-    let result = args
-
-    /* RESOLVING ARGS */
-    if (arg_type !== "PROMISE" && reso) {
-      /**
-       * If some signature needs to deal with both Promises
-       * and non-Promises, non-Promises are wrapped in a
-       * Promise to "lift" them into the proper context for
-       * handling
-       */
-      result = Promise.resolve(args)
-    }
-    if (args !== Object(args) && !sub$) {
-      nosub$_err(c, i)
-      return acc
-    }
-    if (arg_type === "PROMISE") {
-      // result = await discardable(args).catch(e => e)
-      result = await args.catch((e) => e)
-    }
-    if (arg_type === "THUNK") {
-      // if thunk, dispatch to ad-hoc stream, return acc
-      // as-is ⚠ this command will not be waited on
-      result = args()
-      console.log(`dispatching to ad-hoc stream: ${sub$.id}`)
-      sub$.next(result)
-      return acc
-    }
-    // if function, call it with acc and resolve any Promises
-    if (arg_type === "FUNCTION") {
-      let temp = args(acc)
-      result = isPromise(temp) ? await temp.catch((e) => e) : temp
-    }
-    // if object, send the Command as-is and spread into acc
-    if (arg_type === "OBJECT") {
-      if (!sub$) return { ...acc, ...args }
-      out$.next(c)
-      return { ...acc, ...args }
-    }
-
-    /* RESULT HANDLERS */
-    // TODO: 🤔 think harder about the reso/erro handling
-    if (reso) {
-      // promise rejection handler
-      if (result instanceof Error) {
-        if (erro) {
-          let error = erro(acc, result)
-          if (getIn(error, [CMD_SUB$])) return out$.next(error)
-          console.warn(err_str, "Promise rejected:", result)
-          return acc
+    return task_array.reduce(async (a, c, i) => {
+        const acc = await a
+        // console.log("ACCUMULATOR =>", acc)
+        if (isFunction(c)) {
+            try {
+                const recur = c(acc)
+                // ensures accumulator is preserved between stacks
+                recur.unshift({ [CMD_ARGS]: acc })
+                return multiplex(recur)
+            } catch (e) {
+                console.warn(err_str, e)
+                return
+            }
         }
-        console.warn(`no 'erro' (Error handler) set for error in ${result}`)
-      }
-      // resovled promise handler
-      if (!(result instanceof Error)) {
-        let resolved = reso(acc, result)
-        // resolved promise with no sub$ key -> spread
-        // resolved value into acc
-        if (getIn(resolved, [CMD_SUB$])) out$.next(resolved)
-        else if (!sub$) return { ...acc, ...resolved }
-        result = resolved
-      }
-    }
-    // no sub$ key & not a promise -> just spread into acc
-    if (!reso && !sub$) return { ...acc, ...result }
+        const sub$ = c[CMD_SUB$]
+        const args = c[CMD_ARGS]
+        const erro = c[CMD_ERRO]
+        const reso = c[CMD_RESO]
+        // const _source$ = c[source$]
+        // const _handler = c[handler]
+        const knowns = [CMD_SUB$, CMD_ARGS, CMD_RESO, CMD_ERRO, CMD_SRC$, CMD_WORK]
+        const [unknowns] = diff_keys(knowns, c)
 
-    // error, but no error handler
-    if (result instanceof Error) {
-      console.warn(err_str, result)
-      return acc
-    }
-    if (result !== Object(result)) {
-      if (!sub$) {
-        nosub$_err(c, i)
-        return acc
-      }
-      // if the final result is primitive, you can't refer
-      // to this value in proceeding Commands -> send the
-      // Command as-is, return acc as-is.
-      out$.next({ [CMD_SUB$]: sub$, [CMD_ARGS]: result })
-      return acc
-    }
-    // if the result has made it this far, send it along
-    // console.log(`${sub$} made it through`)
-    out$.next({ [CMD_SUB$]: sub$, [CMD_ARGS]: result })
-    return { ...acc, ...result }
-  }, Promise.resolve({}))
+        if (unknowns.length > 0) throw new Error(xKeyError(err_str, c, unknowns, sub$, i))
+        const arg_type = stringify_type(args)
+
+        let result = args
+
+        /* RESOLVING ARGS */
+        if (arg_type !== "PROMISE" && reso) {
+            /**
+             * If some signature needs to deal with both Promises
+             * and non-Promises, non-Promises are wrapped in a
+             * Promise to "lift" them into the proper context for
+             * handling
+             */
+            result = Promise.resolve(args)
+        }
+        if (args !== Object(args) && !sub$) {
+            nosub$_err(c, i)
+            return acc
+        }
+        if (arg_type === "PROMISE") {
+            // result = await discardable(args).catch(e => e)
+            result = await args.catch((e) => e)
+        }
+        if (arg_type === "THUNK") {
+            // if thunk, dispatch to ad-hoc stream, return acc
+            // as-is ⚠ this command will not be waited on
+            result = args()
+            console.log(`dispatching to ad-hoc stream: ${sub$.id}`)
+            sub$.next(result)
+            return acc
+        }
+        // if function, call it with acc and resolve any Promises
+        if (arg_type === "FUNCTION") {
+            let temp = args(acc)
+            result = isPromise(temp) ? await temp.catch((e) => e) : temp
+        }
+        // if object, send the Command as-is and spread into acc
+        if (arg_type === "OBJECT") {
+            if (!sub$) return { ...acc, ...args }
+            out$.next(c)
+            return { ...acc, ...args }
+        }
+
+        /* RESULT HANDLERS */
+        // TODO: 🤔 think harder about the reso/erro handling
+        if (reso) {
+            // promise rejection handler
+            if (result instanceof Error) {
+                if (erro) {
+                    let error = erro(acc, result)
+                    if (getIn(error, [CMD_SUB$])) return out$.next(error)
+                    console.warn(err_str, "Promise rejected:", result)
+                    return acc
+                }
+                console.warn(`no 'erro' (Error handler) set for error in ${result}`)
+            }
+            // resovled promise handler
+            if (!(result instanceof Error)) {
+                let resolved = reso(acc, result)
+                // resolved promise with no sub$ key -> spread
+                // resolved value into acc
+                if (getIn(resolved, [CMD_SUB$])) out$.next(resolved)
+                else if (!sub$) return { ...acc, ...resolved }
+                result = resolved
+            }
+        }
+        // no sub$ key & not a promise -> just spread into acc
+        if (!reso && !sub$) return { ...acc, ...result }
+
+        // error, but no error handler
+        if (result instanceof Error) {
+            console.warn(err_str, result)
+            return acc
+        }
+        if (result !== Object(result)) {
+            if (!sub$) {
+                nosub$_err(c, i)
+                return acc
+            }
+            // if the final result is primitive, you can't refer
+            // to this value in proceeding Commands -> send the
+            // Command as-is, return acc as-is.
+            out$.next({ [CMD_SUB$]: sub$, [CMD_ARGS]: result })
+            return acc
+        }
+        // if the result has made it this far, send it along
+        // console.log(`${sub$} made it through`)
+        out$.next({ [CMD_SUB$]: sub$, [CMD_ARGS]: result })
+        return { ...acc, ...result }
+    }, Promise.resolve({}))
 }
