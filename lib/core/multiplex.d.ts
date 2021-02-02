766a2e3373e50964767488cd757b459435cae6ba
Logan Powell <loganp@tepper.cmu.edu>
2021-02-02 11:20:38 -0500

    switching to subtree directory and running updates

--- lib/core/multiplex.d.ts
+++ lib/core/multiplex.d.ts
@@ -1,6 +1,6 @@
 import { Subscription, PubSub } from "@thi.ng/rstream";
+export declare const multiplex: (out$: any) => (task_array: any) => any;
 export declare const run$: PubSub<any, any>;
 export declare const out$: PubSub<any, any>;
 export declare const cmd$: Subscription<any, any>;
 export declare const task$: Subscription<any, any>;
-export declare function multiplex(task_array: any): any;
