c9750fb82d3c39fec135c46834a01ada25bd63b8
Logan Powell <loganp@tepper.cmu.edu>
2021-02-03 10:24:19 -0500

    switching to subtree directory and running updates

--- lib/core/multiplex.d.ts
+++ lib/core/multiplex.d.ts
@@ -1,4 +1,6 @@
 import { Subscription, PubSub } from "@thi.ng/rstream";
+export declare const keys_match: (C: any) => string;
+export declare const process_args: (acc: any, args: any) => any;
 export declare const multiplex: (out$: any) => (task_array: any) => any;
 export declare const run$: PubSub<any, any>;
 export declare const out$: PubSub<any, any>;
