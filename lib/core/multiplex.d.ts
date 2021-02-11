7f166fd5fc6569571e370e883c2700264efb3729
Logan Powell <loganp@tepper.cmu.edu>
2021-02-11 11:59:16 -0500

    switching to subtree directory and running updates

--- lib/core/multiplex.d.ts
+++ lib/core/multiplex.d.ts
@@ -1,9 +1,12 @@
 import { Subscription, PubSub } from "@thi.ng/rstream";
 export declare const keys_match: (C: any) => string;
 export declare const processArgs: (acc: any, args: any) => any;
-export declare const handlePattern: (acc: any, C: any, out$?: {
-    next: any;
-}, i?: any) => Promise<any>;
+export declare const handlePattern: (acc?: {}, C?: {
+    sub$: any;
+    args: any;
+    reso: any;
+    erro: any;
+}, O$?: PubSub<any, any>, i?: number) => Promise<any>;
 export declare const multiplex: (out$: any) => (task_array: any) => any;
 export declare const run$: PubSub<any, any>;
 export declare const out$: PubSub<any, any>;
--- lib/registers/registerCMD.d.ts
+++ lib/registers/registerCMD.d.ts
@@ -1,7 +1,7 @@
 import { Subscription } from "@thi.ng/rstream";
 import { CMD_SUB$, CMD_ARGS, CMD_RESO, CMD_ERRO, Command } from "@-0/keys";
 export declare const log$: Subscription<any, any>;
-export declare const upstreamCMDInject$: any;
+export declare const forwardUpstreamCMD$: any;
 export declare const registerCMD: (command?: Command) => {
     sub$: any;
     args: any;
