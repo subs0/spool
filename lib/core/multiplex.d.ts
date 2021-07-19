2b1fc149268f95bba0210d34a38b0dad91d00c36
Logan Powell <loganp@tepper.cmu.edu>
2021-07-19 12:14:03 -0400

    switching to subtree directory and running updates

--- lib/core/multiplex.d.ts
+++ lib/core/multiplex.d.ts
@@ -7,7 +7,7 @@ export declare const handlePattern: (acc?: {}, C?: {
     reso: any;
     erro: any;
 }, O$?: PubSub<any, any, any>, i?: number) => Promise<any>;
-export declare const multiplex: (out$: any) => (task_array: any) => any;
+export declare const multiplex: (_out$: any) => (task_array: any) => any;
 export declare const run$: PubSub<any, any>;
 export declare const out$: PubSub<any, any>;
 export declare const cmd$: ISubscription<any, any>;
--- lib/registers/registerCMD.d.ts
+++ lib/registers/registerCMD.d.ts
@@ -1,15 +1,5 @@
-import { ISubscribable, Subscription, ISubscription, PubSub } from "@thi.ng/rstream";
+import { Subscription, ISubscription, PubSub } from "@thi.ng/rstream";
 import { Command, ICommand } from "@-0/keys";
 export declare const log$: Subscription<any, any>;
 export declare const forwardUpstreamCMD$: (command: Command, downstream: PubSub<any>) => ISubscription<any, any>;
-export declare const registerCMD: (command?: ICommand, dev?: boolean) => {
-    sub$: string;
-    args: any;
-    reso: (acc: {}, res: {}) => any;
-    erro: (acc: {}, err: Error, out$: ISubscribable<any>) => any;
-} | {
-    sub$: string;
-    args: any;
-    reso?: undefined;
-    erro?: undefined;
-};
+export declare const registerCMD: (command?: ICommand, dev?: boolean) => Command;
