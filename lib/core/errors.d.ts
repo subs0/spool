65712f502d4969b9475389d85b3427f273571e9d
Logan Powell <loganp@tepper.cmu.edu>
2021-07-22 14:32:47 -0400

    switching to subtree directory and running updates

---
+++ lib/core/errors.d.ts
@@ -0,0 +1,6 @@
+export declare const err_str = "\uD83D\uDD25 Command Dispatch Interrupted \uD83D\uDD25";
+export declare const noSubEr: (c: any, i: any) => string;
+export declare const noEroEr: (c: any, i: any) => string;
+export declare const task_not_array_error: (x: any) => string;
+export declare const no_args_error: (C: any, i?: any) => string;
+export declare const NA_keys: (c: any, i: any) => string;
