3b1f1848f7e6721d986f32b8cbb97f08848fd82c
Logan Powell <loganp@tepper.cmu.edu>
2021-08-16 18:30:17 -0400

    syncing supertree with subtree

--- lib/registers/registerCMD.d.ts
+++
@@ -1,5 +0,0 @@
-import { Subscription, PubSub } from "@thi.ng/rstream";
-import { Command, ICommand } from "@-0/keys";
-export declare const log$: Subscription<any, any>;
-export declare const forwardUpstreamCMD$: (command: Command, downstream: PubSub<any>) => any;
-export declare const registerCMD: (command?: ICommand, dev?: boolean) => Command;
