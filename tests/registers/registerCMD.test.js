f2f7dca0e9e8fb126510da20ace8af7d7d56d554
Logan Powell <loganp@tepper.cmu.edu>
2021-08-23 11:03:22 -0400

    syncing supertree with subtree

--- tests/registers/registerCMD.test.js
+++ tests/registers/registerCMD.test.js
@@ -4,28 +4,28 @@ import { CMD_SUB$, CMD_ARGS, CMD_RESO, CMD_ERRO, CMD_SRC$, CMD_WORK } from "@-0/
 import { log$, registerCMD } from "../../src/registers"
 import { run$, out$ } from "../../src/core"
 
-const warned = (x = jest.fn()) => (jest.spyOn(console, "warn").mockImplementation(x), x)
+//const warned = (x = jest.fn()) => (jest.spyOn(console, "warn").mockImplementation(x), x)
 
 describe(`registerCMD:`, () => {
     test(`1: Command registration returns Command without \`${CMD_WORK}\` side-effect handler`, async () => {
         const TEST = registerCMD({
-            [CMD_SUB$] : "TEST",
-            [CMD_ARGS] : { fire: "🔥" },
-            [CMD_WORK] : x => x
+            [CMD_SUB$]: "TEST",
+            [CMD_ARGS]: { fire: "🔥" },
+            [CMD_WORK]: x => x,
         })
 
         expect(TEST).toMatchObject({
-            [CMD_SUB$] : "TEST",
-            [CMD_ARGS] : { fire: "🔥" }
+            [CMD_SUB$]: "TEST",
+            [CMD_ARGS]: { fire: "🔥" },
         })
     })
     test(`2: run$'ing a Command triggers \`${CMD_WORK}\` side-effect handler`, async () => {
         const relay = jest.fn(x => x)
 
         const TEST2 = registerCMD({
-            [CMD_SUB$] : "TEST2",
-            [CMD_ARGS] : { fire: "🔥" },
-            [CMD_WORK] : ({ fire }) => relay({ rocket: fire + "🚀" })
+            [CMD_SUB$]: "TEST2",
+            [CMD_ARGS]: { fire: "🔥" },
+            [CMD_WORK]: ({ fire }) => relay({ rocket: fire + "🚀" }),
         })
 
         await run$.next(TEST2)
@@ -37,10 +37,10 @@ describe(`registerCMD:`, () => {
     test(`3: Attempting to register a Command with unknown key(s) throw Error`, async () => {
         const TEST4 = () =>
             registerCMD({
-                [CMD_SUB$] : "TEST3",
-                [CMD_ARGS] : { fire: "🔥" },
-                [CMD_WORK] : jest.fn(({ fire }) => ({ rocket: fire + "🚀" })),
-                unknownKey : "breaks stuff"
+                [CMD_SUB$]: "TEST3",
+                [CMD_ARGS]: { fire: "🔥" },
+                [CMD_WORK]: jest.fn(({ fire }) => ({ rocket: fire + "🚀" })),
+                unknownKey: "breaks stuff",
             })
 
         expect(TEST4).toThrow(Error)
@@ -48,8 +48,8 @@ describe(`registerCMD:`, () => {
     test(`4: registering without \`${CMD_WORK}\` && \`${CMD_SRC$}\` keys throws Error`, async () => {
         const TEST5 = () =>
             registerCMD({
-                [CMD_SUB$] : "TEST4",
-                [CMD_ARGS] : { fire: "🔥" }
+                [CMD_SUB$]: "TEST4",
+                [CMD_ARGS]: { fire: "🔥" },
             })
 
         expect(TEST5).toThrow(Error)
@@ -58,11 +58,11 @@ describe(`registerCMD:`, () => {
         const s$ = stream({ id: "registerCMD/5" })
         const relay = jest.fn(x => x)
 
-        const TEST3 = registerCMD({
-            [CMD_SUB$] : "TEST5",
-            [CMD_ARGS] : { fire: "🔥" }, //static = same payload every time
-            [CMD_WORK] : ({ fire }) => relay({ rocket: fire + "🚀" }),
-            [CMD_SRC$] : s$
+        registerCMD({
+            [CMD_SUB$]: "TEST5",
+            [CMD_ARGS]: { fire: "🔥" }, //static = same payload every time
+            [CMD_WORK]: ({ fire }) => relay({ rocket: fire + "🚀" }),
+            [CMD_SRC$]: s$,
         })
 
         await s$.next({ this_doesnt_matter: "💦" })
@@ -77,15 +77,15 @@ describe(`registerCMD:`, () => {
             return x
         })
 
-        const TEST3 = registerCMD({
-            [CMD_SUB$] : "TEST6",
-            [CMD_ARGS] : ({ this_will_be_transformed }) => {
+        registerCMD({
+            [CMD_SUB$]: "TEST6",
+            [CMD_ARGS]: ({ this_will_be_transformed }) => {
                 return { fire: this_will_be_transformed }
-                console.log("thrown in this_will_be_transformed registerCMD/6:", x)
-                throw new Error("funk!")
+                //console.log("thrown in this_will_be_transformed registerCMD/6:", x)
+                //throw new Error("funk!")
             },
-            [CMD_WORK] : ({ fire }) => relay({ rocket: fire + "🚀" }),
-            [CMD_SRC$] : s$
+            [CMD_WORK]: ({ fire }) => relay({ rocket: fire + "🚀" }),
+            [CMD_SRC$]: s$,
         })
 
         await s$.next({ this_will_be_transformed: "🔥" })
