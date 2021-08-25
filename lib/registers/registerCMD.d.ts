368cbcb4f6d6f175a280163f9f0e07832bc7a26f
Logan Powell <loganp@tepper.cmu.edu>
2021-08-25 16:06:01 -0400

    fixes type issues

--- lib/registers/registerCMD.d.ts
+++ lib/registers/registerCMD.d.ts
@@ -1,4 +1,4 @@
 import { ICommand, ICommandObject } from "@-0/keys";
 export declare const log$: import("@thi.ng/rstream").Stream<unknown>;
 export declare const forwardUpstreamCMD$: (command: any, downstream: any) => any;
-export declare const registerCMD: (command: ICommand, dev?: boolean) => ICommandObject;
+export declare const registerCMD: (command: ICommand, dev?: boolean) => ICommandObject | ICommand;
